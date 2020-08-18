1ced9dfa8edd92b9bd9bf04491a6abedccb40845
Robson Gomes <60051587+robson-weish@users.noreply.github.com>
2020-08-18 01:16:56 -0300

    Part of the course

---
+++ Curso-03/Variaveis_Comentarios/Desafio/index.js
@@ -0,0 +1,22 @@
+var a = 10
+
+document.write (a+'<br>')
+
+var b = 20
+
+document.write (b+'<br>')
+
+var c = null
+
+document.write (c+'<br>')
+
+c = a
+a = b
+b = c
+c = null
+
+document.write (a+'<br>')
+
+document.write (b+'<br>')
+
+document.write (c+'<br>')
---
+++ Curso-03/Variaveis_Comentarios/index.js
@@ -0,0 +1,104 @@
+/* COMEÇO DA AULA DE COMENTARIOS 
+ESSA PARTE E A AULA DE COMENTARIOS - Não apagar */
+
+// Comentario de uma linha 
+
+/*
+    Comentarios de múltiplas linhas
+    Ese tipo de comentario permite que várias
+    linhas sejam encapsuladas em um mesmo
+    comentario 
+*/
+
+// FINAL DA AULA DE COMENTARIOS 
+
+/* ESSE E O COMEÇO DA AULA DE VARIAVEIS QUE FICARÁ JUNTO
+AQUI COMEÇA A EXPLICAR O QUE E VARIAVEL E COMO ELA FUNCIONA */
+
+/*
+    var curso
+    var Curso
+    var CURSO
+
+    São três formas de colocar uma variável chamada de curso DIFERENTES, ambas as três formas
+    apresenta uma forma diferente de declarar essas variaveis colocando o mesmo nome.
+*/
+
+/*
+    var nome 
+    var AulaVariaveis
+    var _idade
+
+    Três formas corretas de declarar uma variavel corretamente. 
+*/
+
+/*
+    var 123nome
+    var número
+
+    Duas formas incorretas, não pode iniciar com número e nem ter acento.
+*/
+
+// TIPOS DE VARIAVEIS
+
+// String
+// O operador = e um operador de ATRIBUIÇÃO 
+
+var texto = 'Curso de JavaScript'
+
+// Number
+// float , double ou ponto flutuante = Numeros que possuem fração ou que possuem ponto.
+
+var numeroInteiro = -9
+var numeroFracionado = 11210.75
+
+// Boolean 
+// TRUE e FALSE 
+
+var teste = true 
+
+ /* COMENTADO A PARTE DE DEBUG
+
+alert(texto) // Alert pode ser usado para atribuir um valor de variável.
+
+document.write(texto) // Usado para escrever algo no HTML. "WRITE" - E usado para escrever um texto no HTML 
+
+console.log(numeroInteiro) // Ir no inspecionar e console e ver o resultado e pode ser usado no DeBug também. 
+
+*/
+
+/* 
+    CONCATENAÇÃO 
+    UTILIZANDO UMA JUNÇÃO DE NOMES 
+*/
+
+var nome = prompt('Digite seu nome: ')
+
+document.write(' <br> <h1> Olá ' + nome + ' , tudo bem ? </h1> <br>')
+
+/* 
+    CONCATENAÇÃO 
+    Realizando uma soma 
+*/
+
+var n1 = 3
+var n2 = 3
+
+document.write(n1 + n2)
+
+
+/*
+    Valores NULL e Undefined 
+
+    Valores Null
+        Estamos indicando intencionalmente que queremos ter a ausência do valor.
+
+    Valores Undefined
+        Foi declarada uma variavel, mas que foi omitido um valor nela. 
+
+*/
+
+/* 
+    Alterando o valor de variáveis 
+*/
+
