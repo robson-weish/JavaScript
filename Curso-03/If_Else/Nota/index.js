d9faee4973347aa9b6844e9620de87933493c725
Robson Gomes <60051587+robson-weish@users.noreply.github.com>
2020-08-24 23:56:29 -0300

    If / Else

---
+++ Curso-03/If_Else/Nota/index.js
@@ -0,0 +1,17 @@
+var A1 = parseInt(prompt ('Digite A1'))
+var A2 = parseInt(prompt ('Digite A2'))
+var A3 
+
+A3 = ( A1 + A2 ) / 2
+
+console.log(A3)
+console.log(A2)
+console.log(A1)
+
+if ( A3 >= 6) {
+    document.write('O VALOR DE A3 = ' + A3) 
+    alert (' PASSOU ')
+} else {
+    document.write('O VALOR DE A3 = ' + A3)
+    alert (' SE LASCOU ')
+}
