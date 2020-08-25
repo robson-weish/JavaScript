19463e4baf99249eb7e566de7328b87409e751a3
Robson Gomes <60051587+robson-weish@users.noreply.github.com>
2020-08-25 00:37:31 -0300

    If / Else

---
+++ Curso-03/If_Else/index.js
@@ -0,0 +1,13 @@
+if ( 2 >= 1 && 3 >= 1 && 'a' != 'b'){
+    document.write('Verdadeiro <br>')
+}else{
+    document.write('Falso <br>')
+}
+
+if ( 2 <= 1 || 3 <= 2 ){
+    document.write('Verdadeiro <br>')
+}else{
+    document.write('Falso <br>')
+}
+
+
