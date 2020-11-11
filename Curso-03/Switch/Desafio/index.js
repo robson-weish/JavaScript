c8c6f869be3e2392393c3ddc99c7ec1a62c0dcd6
Robson Gomes <60051587+robson-weish@users.noreply.github.com>
2020-11-11 13:42:05 -0300

    Correction change

--- Curso-03/Switch/Desafio/index.js
+++ Curso-03/Switch/Desafio/index.js
@@ -9,6 +9,7 @@ altura = cen / 100
 console.log(altura);
 
 massa = peso/(altura*altura);
+console.log(massa)
 
 /* 
 
@@ -17,36 +18,31 @@ console.log(massa)
 
 */
 
-switch (massa) {
-    case ' massa > 0 ':
+    if (  massa < 0 ){ 
         alert('caiu')
-        break
-    case massa >= 16 && massa < 16.99:
+    }else if ( massa <= 16 && massa < 16.99 ){ 
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ')
         document.write('Baixo peso grave <br>')
-        break
-    case massa >= 17 && massa < 18.49:
+    }else if ( massa <= 17 && massa < 18.49 ) { 
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ')
         document.write('Baixo peso <br>')
-        break
-    case massa >= 18.50 && massa < 24.99:
+    }else if( massa <= 18.50 && massa < 24.99 ){  
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ')
         document.write('Peso normal <br>')
-        break
-    case massa >= 25 && massa < 29.99:
+    }else if ( massa <= 25 && massa < 29.99 ){  
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ')
         document.write('Sobrepeso <br>')
-        break
-    case massa >= 30 && massa < 34.99:
+    }else if (  massa <= 30 && massa < 34.99 ) { 
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ')
         document.write('Obesidade grau I <br>')
-        break
-    case massa >= 35 && massa < 39.99:
+    }else if ( massa <= 35 && massa < 39.99 ){ 
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ')
         document.write('Obesidade grau II <br>')
-        break
-    case massa > 40:
+    }else if (  massa < 40 ) { 
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ')
         document.write('Obesidade grau III  <br>')
-        break
-}
+    }else { 
+        alert('Error')
+    }
+        
+
